80ba61e408c18dcca5344267ff7204b38781a6c6
Azeen Ali <traximuser20@gmail.com>
2026-09-05 00:59:52 +0500

    --ui-changes

--- src/pages/VotePage.vue
+++ src/pages/VotePage.vue
@@ -66,7 +66,7 @@ function triggerShake() {
       <VoteCard
         animal="cat"
         emoji="🐱"
-        label="Pussy"
+        label="Billi"
         description="Independent, curious, and secretly plotting world domination"
         :votes="store.votes.cat"
         :has-voted="store.hasVoted"
@@ -79,7 +79,7 @@ function triggerShake() {
       <VoteCard
         animal="dog"
         emoji="🐶"
-        label="Madarchod"
+        label="Doggo"
         description="Loyal, playful, and always happy to see you"
         :votes="store.votes.dog"
         :has-voted="store.hasVoted"
